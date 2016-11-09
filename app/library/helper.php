2c4d97a8a6ba227627b227948d78ab1beab1137d
李铭昕 <715557344@qq.com>
2016-11-09 10:26:58 +0800

    * 增加ajax success 和 error 返回

--- app/library/helper.php
+++ app/library/helper.php
@@ -9,6 +9,7 @@
 // | Date: 2016/11/9 Time: 9:55
 // +----------------------------------------------------------------------
 use limx\func\Debug;
+use limx\phalcon\Ajax;
 
 if (!function_exists('dump')) {
     /**
@@ -20,5 +21,35 @@ if (!function_exists('dump')) {
     function dump($data)
     {
         Debug::dump($data);
+    }
+}
+
+if (!function_exists('success')) {
+
+    /**
+     * [success desc]
+     * @desc
+     * @author limx
+     * @param $data
+     * @return \limx\phalcon\JsonResponse
+     */
+    function success($data)
+    {
+        return Ajax::success($data);
+    }
+}
+
+if (!function_exists('error')) {
+
+    /**
+     * [error desc]
+     * @desc
+     * @author limx
+     * @param $data
+     * @return \limx\phalcon\JsonResponse
+     */
+    function error($data)
+    {
+        return Ajax::error($data);
     }
 }
