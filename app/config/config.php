ce962ed7f8f795a8d3fc58a55fbdf7db0fb501f0
李铭昕 <715557344@qq.com>
2016-11-10 22:38:28 +0800

    * 修改版本号

--- app/config/config.php
+++ app/config/config.php
@@ -10,7 +10,7 @@ use Phalcon\Config;
 
 return new Config(
     [
-        'version' => '1.0.9',
+        'version' => '1.0.10',
         'database' => [
             'adapter' => 'Mysql',
             'host' => 'localhost',
