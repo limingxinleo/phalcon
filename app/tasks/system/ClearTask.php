3fe27b44edb508c11f8a2343af1f229896b06ee8
李铭昕 <715557344@qq.com>
2016-11-14 13:59:56 +0800

    * 增加删除缓存的脚本

---
+++ app/tasks/system/ClearTask.php
@@ -0,0 +1,61 @@
+<?php
+// +----------------------------------------------------------------------
+// | Demo [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016 http://www.lmx0536.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: limx <715557344@qq.com> <http://www.lmx0536.cn>
+// +----------------------------------------------------------------------
+// | Date: 2016/11/14 Time: 13:39
+// +----------------------------------------------------------------------
+namespace MyApp\Tasks\System;
+
+use Phalcon\Cli\Task;
+use limx\func\File;
+
+class ClearTask extends Task
+{
+    public function mainAction()
+    {
+        echo "Please input the clear action" . PHP_EOL;
+    }
+
+    public function dataAction($params = [])
+    {
+        $dir = di('config')->application->cacheDir . 'data/';
+        if (strtolower($params[0]) !== 'yes') {
+            echo 'Do you want to delete cache of view?(yes or no)' . PHP_EOL;
+            echo $dir . PHP_EOL;
+            $arg = trim(fgets(STDIN));
+            if (strtolower($arg) !== 'yes') {
+                return;
+            }
+        }
+        // 删除缓存
+        File::rm($dir, false);
+        echo 'delete cache success' . PHP_EOL;
+    }
+
+    /**
+     * [viewAction desc]
+     * @desc 清理视图缓存
+     * @author limx
+     * @param array $params
+     */
+    public function viewAction($params = [])
+    {
+        $dir = di('config')->application->cacheDir . 'view/';
+        if (strtolower($params[0]) !== 'yes') {
+            echo 'Do you want to delete cache of view?(yes or no)' . PHP_EOL;
+            echo $dir . PHP_EOL;
+            $arg = trim(fgets(STDIN));
+            if (strtolower($arg) !== 'yes') {
+                return;
+            }
+        }
+        // 删除缓存
+        File::rm($dir, false);
+        echo 'delete cache success' . PHP_EOL;
+    }
+
+}
