b9d0ba19e91bec15815eb7070a4ee266467b1d48
李铭昕 <715557344@qq.com>
2016-11-17 14:29:19 +0800

    * 增加request获取数据测试

--- app/controllers/test/IndexController.php
+++ app/controllers/test/IndexController.php
@@ -139,7 +139,29 @@ class IndexController extends ControllerBase
         $url = url('index/getParams', ['key1' => 1111, 'key2' => 222]);
         dump($url);
 
+        $url = $this->request->getURI();
+        dump($url);
+    }
 
+    public function requestAction()
+    {
+        $request = $this->request;
+
+        dump($request->get());                          //默认获取所有的请求参数返回的是array效果和获取$_REQUEST相同
+        dump($request->get('wen'));                     //获取摸个特定请求参数key的valuer和$_REQUEST['key']相同
+        dump($request->getQuery('url', null, 'url'));   //获取get请求参数,第二个参数为过滤类型,第三个参数为默认值
+        dump($request->getMethod());                    //获取请求的类型如果是post请求会返回"POST"
+        dump($request->isAjax());                       //判断请求是否为Ajax请求
+        dump($request->isPost());                       //判断是否是Post请求类似的有(isGet,isPut,isPatch,isHead,isDelete,isOptions等)
+        dump($request->getHeaders());                   //获取所有的Header,返回结果为数组
+        dump($request->getHeader('Content-Type'));      //获取Header中的的莫一个指定key的指
+        dump($request->getURI());                       //获取请求的URL比如phalcon.w-blog.cn/phalcon/Request获取的/phalcon/Request
+        dump($request->getHttpHost());                  //获取请求服务器的host比如phalcon.w-blog.cn/phalcon/Request获取的phalcon.w-blog.cn
+        dump($request->getServerAddress());             //获取当前服务器的IP地址
+        dump($request->getRawBody());                   //获取Raw请求json字符
+        dump($request->getJsonRawBody());               //获取Raw请求json字符并且转换成数组对象
+        dump($request->getScheme());                    //获取请求是http请求还是https请求
+        dump($request->getServer('REMOTE_ADDR'));       //等同于$_SERVER['REMOTE_ADDR']
     }
 
     public function getParamsAction()
