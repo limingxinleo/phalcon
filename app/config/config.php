72f122414694e5a9e14c894e28d8e1744d91c475
李铭昕 <715557344@qq.com>
2016-11-24 13:31:02 +0800

    * 修改版本号

--- app/config/config.php
+++ app/config/config.php
@@ -27,7 +27,7 @@ return new Config(
         | This value is version for this project.
         |
         */
-        'version' => '1.1.6',
+        'version' => '1.1.7',
 
         /*
         |--------------------------------------------------------------------------
