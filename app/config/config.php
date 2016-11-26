dc664cc4e4510922e841b9bf5699d864c12fa0bd
李铭昕 <715557344@qq.com>
2016-11-26 16:27:58 +0800

    * 发布版本

--- app/config/config.php
+++ app/config/config.php
@@ -27,7 +27,7 @@ return new Config(
         | This value is version for this project.
         |
         */
-        'version' => '1.2.1',
+        'version' => '1.2.2',
 
         /*
         |--------------------------------------------------------------------------
