7ccf90e706da0780a13e3e46a1135302245bf2d8
李铭昕 <715557344@qq.com>
2016-12-05 21:43:20 +0800

    * 增加phpinfo

--- app/controllers/test/IndexController.php
+++ app/controllers/test/IndexController.php
@@ -14,6 +14,11 @@ class IndexController extends ControllerBase
         dump($this->settings);
     }
 
+    public function infoAction()
+    {
+        echo phpinfo();
+    }
+
     public function vueAction()
     {
         return $this->view->render('test/index', 'vue');
