2e29fde9cef865906a63908ba4374119307858f9
李铭昕 <715557344@qq.com>
2016-12-11 10:26:13 +0800

    * 增加阿里短信测试

--- app/controllers/test/AliController.php
+++ app/controllers/test/AliController.php
@@ -2,6 +2,8 @@
 
 namespace MyApp\Controllers\Test;
 
+use Sms\Request\V20160927 as Sms;
+
 class AliController extends ControllerBase
 {
 
@@ -90,6 +92,34 @@ class AliController extends ControllerBase
     {
         $res = $this->request->get();
         dump($res);
+    }
+
+    /**
+     * [smsAction desc]
+     * @desc 下载阿里短信官方sdk
+     * @author limx
+     */
+    public function smsAction()
+    {
+        library('alisms/aliyun-php-sdk-core/Config.php');
+
+        $iClientProfile = \DefaultProfile::getProfile("cn-hangzhou", env('ALIYUN_ACCESS_KEY'), env('ALIYUN_ACCESS_SECRET'));
+        $client = new \DefaultAcsClient($iClientProfile);
+        $request = new Sms\SingleSendSmsRequest();
+        $request->setSignName("祎昕测试");/*签名名称*/
+        $request->setTemplateCode("SMS_33520803");/*模板code*/
+        $request->setRecNum("13250874521");/*目标手机号*/
+        $request->setParamString("{\"name\":\"李铭昕\"}");/*模板变量，数字一定要转换为字符串*/
+        try {
+            $response = $client->getAcsResponse($request);
+            print_r($response);
+        } catch (ClientException  $e) {
+            print_r($e->getErrorCode());
+            print_r($e->getErrorMessage());
+        } catch (ServerException  $e) {
+            print_r($e->getErrorCode());
+            print_r($e->getErrorMessage());
+        }
     }
 
 }
--- app/views/test/index.volt
+++ app/views/test/index.volt
@@ -57,11 +57,12 @@
             </div>
             <div class="col-md-12">
                 <h3>
-                    支付宝相关
+                    阿里相关
                 </h3>
                 <a type="button" class="btn btn-default" href="{{ url('test/ali/index') }}">WAP支付</a>
                 <a type="button" class="btn btn-default" href="{{ url('test/ali/info') }}">用户信息</a>
                 <a type="button" class="btn btn-default" href="{{ url('test/ali/checkSign') }}">验签接口</a>
+                <a type="button" class="btn btn-default" href="{{ url('test/ali/sms') }}">短信接口</a>
             </div>
         </div>
     </div>
