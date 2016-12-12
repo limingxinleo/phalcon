c8af08a74eab28efaa92955c1c03924451ae2f8a
李铭昕 <715557344@qq.com>
2016-12-12 16:50:35 +0800

    * 增加七牛 直播测试

--- app/controllers/test/QiniuController.php
+++ app/controllers/test/QiniuController.php
@@ -2,13 +2,70 @@
 
 namespace MyApp\Controllers\Test;
 
+use limx\func\Encoder;
+
 class QiniuController extends \Phalcon\Mvc\Controller
 {
 
-    public function indexAction()
+    public function createAction()
     {
-        echo 1;
+        // Instantiate an Hub object
+        $credentials = new \Qiniu\Credentials(env('QINIU_ACCESS_KEY'), env('QINIU_SECRET_KEY')); #=> Credentials Object
+        $hub = new \Pili\Hub($credentials, env('QINIU_HUB')); # => Hub Object
+        $title = 'TEST';     // optional, auto-generated as default
+        $publishKey = '584e52499e353';     // optional, auto-generated as default
+        $publishSecurity = 'dynamic';     // optional, can be "dynamic" or "static", "dynamic" as default
+
+        $stream = $hub->createStream($title, $publishKey, $publishSecurity); # => Stream Object
+
+        echo "createStream() =>\n";
+        dump($stream);
+        echo "\n\n";
     }
+
+    public function getAction()
+    {
+        // Instantiate an Hub object
+        $credentials = new \Qiniu\Credentials(env('QINIU_ACCESS_KEY'), env('QINIU_SECRET_KEY')); #=> Credentials Object
+        $hub = new \Pili\Hub($credentials, env('QINIU_HUB')); # => Hub Object
+
+        $streamId = 'z1.peppertv.TEST';
+
+        $stream = $hub->getStream($streamId); # => Stream Object
+
+        echo "getStream() =>\n";
+        dump($stream);
+        echo "\n\n";
+    }
+
+    public function roomAction()
+    {
+        library('pili-sdk-php-master/lib/Pili.php');
+        $mac = new \Qiniu\Credentials(env('QINIU_ACCESS_KEY'), env('QINIU_SECRET_KEY')); #=> Credentials Object
+        $client = new \Pili\RoomClient($mac);
+        $resp = $client->createRoom(1, "testroom");
+        //dump($resp);
+
+        //鉴权的有效时间: 1个小时.
+        $resp = $client->roomToken("testroom", 1, 'admin', (time() + 3600));
+        dump($resp);
+
+        $resp = $client->roomToken("testroom", 2, 'user', (time() + 3600));
+        dump($resp);
+
+        $resp = $client->getRoom("testroom");
+        //dump($resp);
+
+//        $resp = $client->deleteRoom("testroom");
+//        print_r($resp);
+
+    }
+
+    public function QiniuToken()
+    {
+
+    }
+
 
 }
 
