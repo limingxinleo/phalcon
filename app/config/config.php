3338946c47cc710c4ae9e053e9c941b62c46fc44
李铭昕 <715557344@qq.com>
2016-12-27 10:01:16 +0800

    * 发布新版本1.4.0 composer更名

--- app/config/config.php
+++ app/config/config.php
@@ -27,7 +27,7 @@ return new Config(
         | This value is version for this project.
         |
         */
-        'version' => '1.3.3',
+        'version' => '1.4.0',
 
         /*
         |--------------------------------------------------------------------------
