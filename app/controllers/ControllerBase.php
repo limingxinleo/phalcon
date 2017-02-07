8380822f646e9761f3940d44448eedf157400e3a
李铭昕 <715557344@qq.com>
2017-02-07 12:25:28 +0800

    * 默认增加Response Trait

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -3,9 +3,12 @@
 namespace MyApp\Controllers;
 
 use Phalcon\Mvc\Controller;
+use MyApp\Traits\System\Response;
 
 class ControllerBase extends Controller
 {
+    use Response;
+
     public function initialize()
     {
     }
