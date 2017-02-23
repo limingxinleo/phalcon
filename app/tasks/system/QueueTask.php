fee827cc6c13e79bef47e4c54f78f375208219f2
李铭昕 <715557344@qq.com>
2017-02-23 16:15:23 +0800

    * 增加消息队列抽象类

---
+++ app/tasks/system/QueueTask.php
@@ -0,0 +1,109 @@
+<?php
+// +----------------------------------------------------------------------
+// | 消息队列 REDIS [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016 http://www.lmx0536.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: limx <715557344@qq.com> <http://www.lmx0536.cn>
+// +----------------------------------------------------------------------
+// | Date: 2017/2/4 Time: 上午10:00
+// +----------------------------------------------------------------------
+declare(ticks = 1);
+namespace MyApp\Tasks\System;
+
+use Phalcon\Cli\Task;
+
+abstract class QueueTask extends Task
+{
+    // 最大进程数
+    protected $maxProcesses = 500;
+    // 当前进程数
+    protected $process = 0;
+    // 消息队列Redis键值
+    protected $queueKey = 'phalcon:test:queue';
+    // 等待时间
+    protected $waittime = 1;
+
+    public function mainAction()
+    {
+        if (!extension_loaded('swoole')) {
+            echo "没有安装swoole扩展" . PHP_EOL;
+            return;
+        }
+        // install signal handler for dead kids
+        pcntl_signal(SIGCHLD, [$this, "signalHandler"]);
+        set_time_limit(0);
+
+        while (true) {
+            if ($this->process < $this->maxProcesses) {
+                $redis = $this->redisClient();
+                $data = $redis->brpop($this->queueKey, 3);//无任务时,阻塞等待
+                if (!$data) {
+                    continue;
+                }
+                if ($data[0] != $this->queueKey) {
+                    // 消息队列KEY值不匹配
+                    continue;
+                }
+                if (isset($data[1])) {
+                    $process = new \swoole_process([$this, 'task']);
+                    $process->write($data[1]);
+                    $pid = $process->start();
+                    if ($pid === false) {
+                        $redis->lpush($this->queueKey, $data[1]);
+                    } else {
+                        $this->process++;
+                    }
+                }
+            } else {
+                if (is_int($this->waittime) && $this->waittime > 0) {
+                    sleep($this->waittime);
+                }
+            }
+        }
+    }
+
+    /**
+     * @desc 子进程
+     * @author limx
+     * @param \swoole_process $worker
+     */
+    public function task(\swoole_process $worker)
+    {
+        swoole_event_add($worker->pipe, function ($pipe) use ($worker) {
+            $recv = $worker->read();            //send data to master
+            $this->run($recv);
+            $worker->exit(0);
+            swoole_event_del($pipe);
+        });
+    }
+
+    /**
+     * @desc 返回redis实例
+     * @author limx
+     * @return mixed
+     */
+    abstract protected function redisClient();
+
+    /**
+     * @desc 消息队列执行的业务代码
+     * @author limx
+     * @return mixed
+     */
+    abstract protected function run($recv);
+
+    /**
+     * @desc 信号处理方法 回收已经dead的子进程
+     * @author limx
+     * @param $signo
+     */
+    private function signalHandler($signo)
+    {
+        switch ($signo) {
+            case SIGCHLD:
+                while ($ret = \swoole_process::wait(false)) {
+                    $this->process--;
+                }
+        }
+    }
+}
