948714f8fc7a71f00348351893a58f2c7f200994
李铭昕 <715557344@qq.com>
2017-02-23 16:28:22 +0800

    * 增加消息队列抽象类

--- app/config/config.php
+++ app/config/config.php
@@ -27,7 +27,7 @@ return new Config(
         | This value is version for this project.
         |
         */
-        'version' => '1.5.6',
+        'version' => '1.5.7',
 
         /*
         |--------------------------------------------------------------------------
