8b364369cdb401945faa2481e09071afed0f6500
李铭昕 <715557344@qq.com>
2017-03-23 10:34:28 +0800

    * 修改session.php

--- app/services/system/session.php
+++ app/services/system/session.php
@@ -18,27 +18,25 @@ if ($config->session->type !== false) {
     $session = null;
     switch ($config->session->type) {
         case 'redis':
-            $redis = $config->redis;
             $session = new SessionRedis([
                 'uniqueId' => $config->unique_id,
-                'host' => $redis->host,
-                'port' => $redis->port,
-                'auth' => $redis->auth,
-                'persistent' => $redis->persistent,
+                'host' => $config->redis->host,
+                'port' => $config->redis->port,
+                'auth' => $config->redis->auth,
+                'persistent' => $config->redis->persistent,
                 'lifetime' => 3600,
                 'prefix' => ':session:',
-                'index' => $redis->index,
+                'index' => $config->redis->index,
             ]);
-            $session->start();
             break;
         case 'file':
         default:
             $session = new SessionAdapter();
-            $session->start();
             break;
 
     }
     if ($session !== null) {
+        $session->start();
         $di->set('session', function () use ($session) {
             return $session;
         });
