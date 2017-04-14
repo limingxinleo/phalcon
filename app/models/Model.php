60d6f68314cb26e9a184fc9adfb0597abfdc732a
李铭昕 <715557344@qq.com>
2017-04-14 20:42:44 +0800

    * 增加model层基类

---
+++ app/models/Model.php
@@ -0,0 +1,22 @@
+<?php
+// +----------------------------------------------------------------------
+// | Model基类 [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016-2017 limingxinleo All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: limx <715557344@qq.com> <http://www.lmx0536.cn>
+// +----------------------------------------------------------------------
+namespace MyApp\Models;
+
+class Model extends \Phalcon\Mvc\Model
+{
+
+    /**
+     * Initialize method for model.
+     */
+    public function initialize()
+    {
+
+    }
+
+}
