521d8e67eee734e56c2f218114459b1553c4477f
李铭昕 <715557344@qq.com>
2017-04-24 10:39:36 +0800

    * 修改日志工具类

--- app/utils/Log.php
+++ app/utils/Log.php
@@ -12,83 +12,83 @@ use limx\phalcon\Logger;
 
 class Log
 {
-    /** @var null 日志目录 */
-    protected $dir = null;
+    /** 日志目录 */
+    const DIR = null;
 
-    /** @var string 文件名 */
-    protected $fileName = "logger.log";
+    /** 文件名 */
+    const FILE_NAME = "logger.log";
 
-    /** @var string 日志类型 */
-    protected $type = "file";
+    /** 日志类型 */
+    const TYPE = "file";
 
-    public function __construct()
+    public static function logger()
     {
-        return Logger::getInstance($this->type, $this->fileName, $this->dir);
+        return Logger::getInstance(static::TYPE, static::FILE_NAME, static::DIR);
     }
 
     public static function log($type, $message = null, array $context = null)
     {
-        $logger = new self();
+        $logger = static::logger();
         return $logger->log($type, $message, $context);
     }
 
     public static function begin()
     {
-        $logger = new self();
+        $logger = static::logger();
         return $logger->begin();
     }
 
     public static function commit()
     {
-        $logger = new self();
+        $logger = static::logger();
         return $logger->commit();
     }
 
     public static function rollback()
     {
-        $logger = new self();
+        $logger = static::logger();
         return $logger->rollback();
     }
 
     public static function debug($message, $context = null)
     {
-        $logger = new self();
+        $logger = static::logger();
         return $logger->debug($message, $context);
     }
 
     public static function error($message, $context = null)
     {
-        $logger = new self();
+        $logger = static::logger();
         return $logger->error($message, $context);
     }
 
     public static function info($message, $context = null)
     {
-        $logger = new self();
+        $logger = static::logger();
         return $logger->info($message, $context);
     }
 
     public static function notice($message, $context = null)
     {
-        $logger = new self();
+        $logger = static::logger();
         return $logger->notice($message, $context);
     }
 
     public static function warning($message, $context = null)
     {
-        $logger = new self();
+        $logger = static::logger();
         return $logger->warning($message, $context);
     }
 
     public static function alert($message, $context = null)
     {
-        $logger = new self();
+        $logger = static::logger();
         return $logger->alert($message, $context);
     }
 
     public static function emergency($message, $context = null)
     {
-        $logger = new self();
+        $logger = static::logger();
         return $logger->emergency($message, $context);
     }
 
