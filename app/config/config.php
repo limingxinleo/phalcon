979684774627bc1f806eeee8822cecb41971d3b5
李铭昕 <715557344@qq.com>
2017-04-30 10:19:49 +0800

    * v1.7.11

--- app/config/config.php
+++ app/config/config.php
@@ -26,7 +26,7 @@ return new Config(
         | This value is version for this project.
         |
         */
-        'version' => '1.7.10',
+        'version' => '1.7.11',
 
         /*
         |--------------------------------------------------------------------------
