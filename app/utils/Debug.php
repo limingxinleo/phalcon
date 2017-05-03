e5f613d942553a72f5dc737bb52e35b390ecec40
李铭昕 <715557344@qq.com>
2017-05-03 13:34:53 +0800

    * 增加debug工具类

---
+++ app/utils/Debug.php
@@ -0,0 +1,23 @@
+<?php
+// +----------------------------------------------------------------------
+// | Debug.php [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016-2017 limingxinleo All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: limx <715557344@qq.com> <https://github.com/limingxinleo>
+// +----------------------------------------------------------------------
+namespace App\Utils;
+class Debug
+{
+    public static function ip()
+    {
+        if (getenv("HTTP_CLIENT_IP"))
+            $ip = getenv("HTTP_CLIENT_IP");
+        else if (getenv("HTTP_X_FORWARDED_FOR"))
+            $ip = getenv("HTTP_X_FORWARDED_FOR");
+        else if (getenv("REMOTE_ADDR"))
+            $ip = getenv("REMOTE_ADDR");
+        else $ip = "Unknow";
+        return $ip;
+    }
+}
