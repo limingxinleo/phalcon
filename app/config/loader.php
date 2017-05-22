75949045a1c9ccc0362cc5522ac63293a0ef79d4
李铭昕 <715557344@qq.com>
2017-05-22 15:37:36 +0800

    * 更新目录结构

--- app/config/loader.php
+++ app/config/loader.php
@@ -17,17 +17,14 @@ $loader
             'App\Controllers' => $config->application->controllersDir,
 
             'App\Listeners' => $config->application->listenersDir,
-            'App\Listeners\System' => $config->application->listenersDir . 'system/',
 
             'App\Logics' => $config->application->logicsDir,
 
             'App\Models' => $config->application->modelsDir,
 
             'App\Tasks' => $config->application->tasksDir,
-            'App\Tasks\System' => $config->application->tasksDir . 'system/',
 
             'App\Traits' => $config->application->traitsDir,
-            'App\Traits\System' => $config->application->traitsDir . 'system/',
 
             'App\Utils' => $config->application->utilsDir,
         ]
