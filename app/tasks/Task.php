09f47c23e52bb3bf6362c68207a4ab9d5f5f052a
李铭昕 <715557344@qq.com>
2017-06-23 16:54:00 +0800

    * Task基类增加三个基础函数

--- app/tasks/Task.php
+++ app/tasks/Task.php
@@ -4,5 +4,28 @@ namespace App\Tasks;
 
 abstract class Task extends \Phalcon\Cli\Task
 {
+    public function onConstruct()
+    {
+        /**
+         * Phalcon\Cli\Task constructor
+         *
+         * public final function __construct()
+         * {
+         *     if method_exists(this, "onConstruct") {
+         *         this->{"onConstruct"}();
+         *     }
+         * }
+         */
+    }
+
+    public function beforeExecuteRoute()
+    {
+        // 在每一个找到的动作前执行
+    }
+
+    public function afterExecuteRoute()
+    {
+        // 在每一个找到的动作后执行
+    }
 }
 
