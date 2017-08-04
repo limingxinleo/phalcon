8e26a2d5b2ca35e46641c8dd57549489682e33e5
李铭昕 <715557344@qq.com>
2017-08-04 13:54:23 +0800

    * 修改 Sql监听者 JSON 为UNICODE编码

--- app/listeners/System/DbListener.php
+++ app/listeners/System/DbListener.php
@@ -76,7 +76,7 @@ class DbListener
 
         $str = PHP_EOL;
         $str .= "SQL语句: " . $sql . PHP_EOL;
-        $str .= "绑定参数: " . json_encode($params) . PHP_EOL;
+        $str .= "绑定参数: " . json_encode($params, JSON_UNESCAPED_UNICODE) . PHP_EOL;
         $str .= "开始时间: " . $begintime . PHP_EOL;
         $str .= "结束时间: " . $endtime . PHP_EOL;
         $str .= "执行时间: " . $runtime . PHP_EOL;
