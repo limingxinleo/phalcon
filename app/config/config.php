da03bceb1e91fffbcf81a50f431631398a46ca0f
李铭昕 <715557344@qq.com>
2017-08-07 09:41:52 +0800

    * 1.10.0

--- app/config/config.php
+++ app/config/config.php
@@ -26,7 +26,7 @@ return new Config(
         | This value is version for this project.
         |
         */
-        'version' => '1.9.15',
+        'version' => '1.10.0',
 
         /*
         |--------------------------------------------------------------------------
