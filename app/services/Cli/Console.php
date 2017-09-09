289a51d0571879876cb8aa9cb30c3241db0fef61
李铭昕 <715557344@qq.com>
2017-09-09 19:15:33 +0800

    * 修改 console服务

--- app/services/Cli/Console.php
+++ app/services/Cli/Console.php
@@ -11,12 +11,15 @@ namespace App\Services\Cli;
 use App\Services\ServiceProviderInterface;
 use Phalcon\Config;
 use Phalcon\DI\FactoryDefault;
+use Phalcon\Cli\Console as ConsoleApp;
 
 class Console implements ServiceProviderInterface
 {
     public function register(FactoryDefault $di, Config $config)
     {
-
+        $di->setShared('console', function () use ($di) {
+            return new ConsoleApp($di);
+        });
     }
 
 }
