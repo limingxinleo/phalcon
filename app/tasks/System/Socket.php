601127ef0ec029ef2d87439bbfcd8feab242e949
李铭昕 <715557344@qq.com>
2017-09-20 10:09:35 +0800

    * 增加swoole server默认配置

--- app/tasks/System/Socket.php
+++ app/tasks/System/Socket.php
@@ -12,13 +12,27 @@ use Phalcon\Cli\Task;
 use limx\phalcon\Cli\Color;
 use swoole_server;
 
+/**
+ * Class Socket
+ * @package App\Tasks\System
+ * @reload  kill -USR1 `cat socket.pid`
+ * @stop    kill `cat socket.pid`
+ * @see     https://wiki.swoole.com/wiki/page/699.html
+ */
 abstract class Socket extends Task
 {
     // 端口号
     protected $port = 11520;
 
     // @see https://wiki.swoole.com/wiki/page/274.html Swoole文档Socket配置选项
-    protected $config = [];
+    protected $config = [
+        'pid_file' => ROOT_PATH . '/socket.pid',
+        'user' => 'nginx',
+        'group' => 'nginx',
+        'daemonize' => false,
+        // 'worker_num' => 8, // cpu核数1-4倍比较合理 不写则为cpu核数
+        'max_request' => 500, // 每个worker进程最大处理请求次数
+    ];
 
     public function mainAction()
     {
