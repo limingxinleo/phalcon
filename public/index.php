de758d4e48f965211a97a0f1fc06157fb0509433
李铭昕 <715557344@qq.com>
2017-09-22 00:23:49 +0800

    * 不再在index.php中捕获错误！

--- public/index.php
+++ public/index.php
@@ -6,23 +6,16 @@ define('IS_CLI', false);
 
 use Phalcon\Mvc\Application;
 
-try {
-    /** Read vendor autoload */
-    if (file_exists(ROOT_PATH . "/vendor/autoload.php")) {
-        include ROOT_PATH . "/vendor/autoload.php";
-    }
-
-    $di = require_once APP_PATH . '/bootstrap.php';
-    /**
-     * Handle the request
-     */
-    $application = new Application($di);
-    $application->useImplicitView(false);
-
-    $application->handle()->send();
-    // echo $application->handle()->getContent();
-} catch (\Exception $e) {
-    $error = $e->getMessage();
-    echo $error;
-    logger($error, 'error', 'error');
+if (file_exists(ROOT_PATH . "/vendor/autoload.php")) {
+    include ROOT_PATH . "/vendor/autoload.php";
 }
+
+$di = require_once APP_PATH . '/bootstrap.php';
+/**
+ * Handle the request
+ */
+$application = new Application($di);
+$application->useImplicitView(false);
+
+$application->handle()->send();
+
