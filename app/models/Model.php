43b2be5e22406b1f0c7604923818ace0b44cffb8
李铭昕 <715557344@qq.com>
2018-01-08 11:03:07 +0800

    删除多余注释

--- app/models/Model.php
+++ app/models/Model.php
@@ -25,13 +25,6 @@ abstract class Model extends \Phalcon\Mvc\Model
      */
     public function initialize()
     {
-        // 模型关系
-        // $options=['alias' => 'user', 'reusable' => true] alias:别名 reusable:模型是否复用
-        // $this->hasOne(...$params, $options = null)
-        // $this->belongsTo(...$params, $options = null)
-        // $this->hasMany(...$params, $options = null)
-        // $this->hasManyToMany(...$params, $options = null)
-
         // Sets if a model must use dynamic update instead of the all-field update
         // $this->useDynamicUpdate(true);
     }
